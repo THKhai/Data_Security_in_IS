f5e7fe2688521f09b016e6d910e5716ff1373c14
THKhai <116781770+THKhai@users.noreply.github.com>
2024-03-03 18:24:08 +0700

    update c5-c7

--- 6_Ora_QLNV_SPs.sql
+++ 6_Ora_QLNV_SPs.sql
@@ -20,7 +20,7 @@ create table DEPT_21127620(
 -- T?o b?ng EMP
 create table EMP_21127620(
     EMPNO number(4) primary key,
-    ENNAME varchar2(10),
+    ENAME varchar2(10),
     JOB varchar2(9),
     MGR number(4),
     HIREDATE date,
@@ -51,20 +51,20 @@ insert into DEPT_21127620(DEPTNO,DNAME,LOC) values (30,'SALES','CHICAGO');
 insert into DEPT_21127620(DEPTNO,DNAME,LOC) values (40,'OPERATIONS','BOSTON');
 
 --insert into EMP_21127620
-insert into EMP_21127620(EMPNO,enname,job,mgr,hiredate,sal,comm,deptno) values (7839,'KING','PRESIDENT',NULL,TO_DATE('17-11-1981','DD-MM-YYYY'),5000,NULL,10);
-insert into EMP_21127620(EMPNO,enname,job,mgr,hiredate,sal,comm,deptno) values (7698,'BLAKE','MANAGER',7839,TO_DATE('01-05-1981','DD-MM-YYYY'),2850,null,30);
-insert into EMP_21127620(EMPNO,enname,job,mgr,hiredate,sal,comm,deptno) values (7782,'CLANK','MANAGER',7839,TO_DATE('09-06-1981','DD-MM-YYYY'),2450,null,10);
-insert into EMP_21127620(EMPNO,enname,job,mgr,hiredate,sal,comm,deptno) values (7566,'JONES','MANAGER',7839,TO_DATE('03-04-1982','DD-MM-YYYY'),2975,null,20);
-insert into EMP_21127620(EMPNO,enname,job,mgr,hiredate,sal,comm,deptno) values (7654,'MARTIN','SALESMAN',7698,TO_DATE('28-09-1981','DD-MM-YYYY'),1250,1400,30);
-insert into EMP_21127620(EMPNO,enname,job,mgr,hiredate,sal,comm,deptno) values (7499,'ALLEN','SALESMAN',7698,TO_DATE('20-02-1981','DD-MM-YYYY'),1600,300,30);
-insert into EMP_21127620(EMPNO,enname,job,mgr,hiredate,sal,comm,deptno) values (7844,'TURNER','SALESMAN',7698,TO_DATE('08-09-1981','DD-MM-YYYY'),1500,0,30);
-insert into EMP_21127620(EMPNO,enname,job,mgr,hiredate,sal,comm,deptno) values (7900,'JAMES', 'CLERK',7698,TO_DATE('03-12-1981','DD-MM-YYYY'),950,null,30);
-insert into EMP_21127620(EMPNO,enname,job,mgr,hiredate,sal,comm,deptno) values (7521,'WARD','SALESMAN',7698,TO_DATE('22-02-1981','DD-MM-YYYY'),1250,500,30);
-insert into EMP_21127620(EMPNO,enname,job,mgr,hiredate,sal,comm,deptno) values (7902,'FORD','ANALYST',7566,TO_DATE('03-12-1981','DD-MM-YYYY'),3000,null,20);
-insert into EMP_21127620(EMPNO,enname,job,mgr,hiredate,sal,comm,deptno) values (7369,'SMITH','CLERK',7902,TO_DATE('17-12-1980','DD-MM-YYYY'),800,null,20);
-insert into EMP_21127620(EMPNO,enname,job,mgr,hiredate,sal,comm,deptno) values (7788, 'SCOTT','ANALYST',7566,TO_DATE('09-12-1982','DD-MM-YYYY'),3000,null,20);
-insert into EMP_21127620(EMPNO,enname,job,mgr,hiredate,sal,comm,deptno) values (7876,'ADAMS','CLERK',7788,TO_DATE('12-01-1983','DD-MM-YYYY'),1100,null,20);
-insert into EMP_21127620(EMPNO,enname,job,mgr,hiredate,sal,comm,deptno) values (7934,'MILLER','CLERK',7782,TO_DATE('23-01-1982','DD-MM-YYYY'),1300,null,10);
+insert into EMP_21127620(EMPNO,ENAME,job,mgr,hiredate,sal,comm,deptno) values (7839,'KING','PRESIDENT',NULL,TO_DATE('17-11-1981','DD-MM-YYYY'),5000,NULL,10);
+insert into EMP_21127620(EMPNO,ENAME,job,mgr,hiredate,sal,comm,deptno) values (7698,'BLAKE','MANAGER',7839,TO_DATE('01-05-1981','DD-MM-YYYY'),2850,null,30);
+insert into EMP_21127620(EMPNO,ENAME,job,mgr,hiredate,sal,comm,deptno) values (7782,'CLANK','MANAGER',7839,TO_DATE('09-06-1981','DD-MM-YYYY'),2450,null,10);
+insert into EMP_21127620(EMPNO,ENAME,job,mgr,hiredate,sal,comm,deptno) values (7566,'JONES','MANAGER',7839,TO_DATE('03-04-1982','DD-MM-YYYY'),2975,null,20);
+insert into EMP_21127620(EMPNO,ENAME,job,mgr,hiredate,sal,comm,deptno) values (7654,'MARTIN','SALESMAN',7698,TO_DATE('28-09-1981','DD-MM-YYYY'),1250,1400,30);
+insert into EMP_21127620(EMPNO,ENAME,job,mgr,hiredate,sal,comm,deptno) values (7499,'ALLEN','SALESMAN',7698,TO_DATE('20-02-1981','DD-MM-YYYY'),1600,300,30);
+insert into EMP_21127620(EMPNO,ENAME,job,mgr,hiredate,sal,comm,deptno) values (7844,'TURNER','SALESMAN',7698,TO_DATE('08-09-1981','DD-MM-YYYY'),1500,0,30);
+insert into EMP_21127620(EMPNO,ENAME,job,mgr,hiredate,sal,comm,deptno) values (7900,'JAMES', 'CLERK',7698,TO_DATE('03-12-1981','DD-MM-YYYY'),950,null,30);
+insert into EMP_21127620(EMPNO,ENAME,job,mgr,hiredate,sal,comm,deptno) values (7521,'WARD','SALESMAN',7698,TO_DATE('22-02-1981','DD-MM-YYYY'),1250,500,30);
+insert into EMP_21127620(EMPNO,ENAME,job,mgr,hiredate,sal,comm,deptno) values (7902,'FORD','ANALYST',7566,TO_DATE('03-12-1981','DD-MM-YYYY'),3000,null,20);
+insert into EMP_21127620(EMPNO,ENAME,job,mgr,hiredate,sal,comm,deptno) values (7369,'SMITH','CLERK',7902,TO_DATE('17-12-1980','DD-MM-YYYY'),800,null,20);
+insert into EMP_21127620(EMPNO,ENAME,job,mgr,hiredate,sal,comm,deptno) values (7788, 'SCOTT','ANALYST',7566,TO_DATE('09-12-1982','DD-MM-YYYY'),3000,null,20);
+insert into EMP_21127620(EMPNO,ENAME,job,mgr,hiredate,sal,comm,deptno) values (7876,'ADAMS','CLERK',7788,TO_DATE('12-01-1983','DD-MM-YYYY'),1100,null,20);
+insert into EMP_21127620(EMPNO,ENAME,job,mgr,hiredate,sal,comm,deptno) values (7934,'MILLER','CLERK',7782,TO_DATE('23-01-1982','DD-MM-YYYY'),1300,null,10);
 
 -- KHANG ( BEGIN )
 CREATE OR REPLACE PROCEDURE usp_C1_ThuNhapNV_21127622
@@ -73,18 +73,14 @@ AS
 BEGIN
     OPEN cursor_ FOR
         SELECT ENAME, SAL
-        FROM EMP_21127622;
+        FROM EMP_21127620;
     DBMS_SQL.RETURN_RESULT(cursor_); 
 END;
-
+/
 BEGIN 
     usp_C1_ThuNhapNV_21127622;
 END;
-/*
-VARIABLE c1 REFCURSOR;
-EXEC usp_C1_ThuNhapNV_21127622( :c1 );
-PRINT c1;    
-*/
+/
 
 CREATE OR REPLACE PROCEDURE usp_C2_CauTrucBangEMP_21127622--(cursor_2 OUT SYS_REFCURSOR)
 AS
@@ -97,15 +93,11 @@ BEGIN
         ORDER BY column_id;
     DBMS_SQL.RETURN_RESULT(cursor_); 
 END;
-
+/
 BEGIN 
     usp_C2_CauTrucBangEMP_21127622;
 END;
-/*
-VARIABLE c2 REFCURSOR;
-EXEC usp_C2_CauTrucBangEMP_21127622( :c2 );
-PRINT c2;  
-*/
+/
 
 CREATE OR REPLACE PROCEDURE usp_C3_SalHireDate_21127622
 (
@@ -123,11 +115,11 @@ BEGIN
       || ' FROM EMP_21127620' USING SALDisplayFormat, HIREDATEFormat;
   DBMS_SQL.RETURN_RESULT(sysRefCursor);
 END;
-
+/
 BEGIN
   usp_C3_SalHireDate_21127622( 'salary', 'hd', '99999999.00', 'YYYY-MM-DD' );
 END;
-
+/
 CREATE OR REPLACE PROCEDURE usp_C4_DanhSachNVLuong_21127622
 AS
     cursor_ SYS_REFCURSOR;
@@ -138,36 +130,67 @@ BEGIN
         WHERE SAL <= 2000 AND SAL >= 1000;
     DBMS_SQL.RETURN_RESULT(cursor_); 
 END;
-
+/
 BEGIN 
     usp_C4_DanhSachNVLuong_21127622;
 END;
+/
 -- KHANG (END)
 
 --TO?N (BEGIN)
+SET SERVEROUTPUT ON;
+
+CREATE OR REPLACE PROCEDURE usp_C5_DanhSachNV_THLL_21127705 
+IS
+BEGIN
+    FOR emp IN (SELECT * FROM EMP_21127620 WHERE EMP_21127620.ENAME LIKE '%LL%' OR EMP_21127620.ENAME LIKE '%TH%')
+    LOOP
+        DBMS_OUTPUT.PUT_LINE('EMPNO: '||emp.EMPNO||' | ENAME: ' || emp.ENAME||' | JOB: '|| emp.JOB||' | MGR: '|| emp.MGR ||' | HIREDATE: '|| emp.HIREDATE||' | SAL: '|| emp.SAL|| ' | COMM: '||emp.COMM|| '| DEPTNO: '|| emp.DEPTNO);
+    END LOOP;
+END;
+/
+--6
+CREATE OR REPLACE PROCEDURE usp_C6_DanhSachNV_VAO1993_21127705 
+IS
+BEGIN
+    FOR emp IN (SELECT * FROM EMP_21127620 WHERE EXTRACT(YEAR FROM EMP_21127620.HIREDATE) = 1993)
+    LOOP
+        DBMS_OUTPUT.PUT_LINE('ENAME: ' || emp.ENAME||' | HIREDATE: '|| emp.HIREDATE||'| DEPTNO: '|| emp.DEPTNO);
+    END LOOP;
+END;
+/
+--7
+CREATE OR REPLACE PROCEDURE usp_C7_DanhSachNV_Luongtang15_21127705 
+IS
+BEGIN
+    FOR emp IN (SELECT * FROM EMP_21127620 WHERE (((EMP_21127620.SAL + EMP_21127620.COMM) / EMP_21127620.SAL) - 1) > 0.25)
+    LOOP
+        DBMS_OUTPUT.PUT_LINE('ENAME: ' || emp.ENAME||' | SAL: '|| emp.SAL||'| DEPTNO: '|| emp.DEPTNO);
+    END LOOP;
+END;
+/
 --TO?N (END)
 
 --KH?I (BEGIN)
-SET SERVEROUTPUT ON;
-
--- cau 10
+-- cau 8
 CREATE OR REPLACE PROCEDURE usp_C8Hienthi_21127620 
 AS
 BEGIN
-    for emp in (select e.enname || '(' || e.job || ')' as EMPLOYEE from EMP_21127620 e)LOOP
+    for emp in (select e.ENAME || '(' || e.job || ')' as EMPLOYEE from EMP_21127620 e)LOOP
     DBMS_OUTPUT.PUT_LINE(emp.EMPLOYEE);
     END LOOP;   
 END;
+/
 exec usp_C8Hienthi_21127620;
 
-select* from emp_21127620;
+
 
 -- cau 9
 CREATE OR REPLACE PROCEDURE  usp_C9_DanhSachNV20_Hienthi_2127620 AS
 BEGIN
-    DBMS_OUTPUT.PUT_LINE('ENName      Date_Hired');    
-    FOR emp IN (select e.enname,e.hiredate from emp_21127620 e where e.deptno = 20) LOOP
-        DBMS_OUTPUT.PUT_LINE(emp.enname || '      ' || TO_CHAR(emp.hiredate, 'DD-MON-YYYY'));
+    DBMS_OUTPUT.PUT_LINE('ENAME      Date_Hired');    
+    FOR emp IN (select e.ENAME,e.hiredate from emp_21127620 e where e.deptno = 20) LOOP
+        DBMS_OUTPUT.PUT_LINE(emp.ENAME || '      ' || TO_CHAR(emp.hiredate, 'DD-MON-YYYY'));
     END LOOP;
 END;
 /
@@ -198,6 +221,6 @@ begin
     DBMS_OUTPUT.PUT_LINE('MIN SALARY: ' || min_sal);
     DBMS_OUTPUT.PUT_LINE('AVERAGE SALARY: ' || avg_sal);
 end;
-
+/
 exec usp_C10_DanhSachNhanVienLuong_21127620;
 --KH?I (END)
