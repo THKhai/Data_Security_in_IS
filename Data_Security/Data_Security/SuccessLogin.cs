dde0145f94faa8e365fe1a7e28340aa25064ac14
THKhai <116781770+THKhai@users.noreply.github.com>
2024-03-22 13:52:41 +0700

    Create đăng nhập

---
+++ Data_Security/Data_Security/SuccessLogin.cs
@@ -0,0 +1,45 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Data_Security
+{
+    public partial class SuccessLogin : Form
+    {
+        string connectString = null;
+        public SuccessLogin(string conStr)
+        {
+            connectString = conStr;
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using(OracleConnection con = new OracleConnection(connectString))
+            {
+                string query = "select username from dba_users;";
+                con.Open();
+                OracleCommand cmd = new OracleCommand(query, con);
+                using(OracleDataAdapter ad =  new OracleDataAdapter(cmd))
+                {
+                    DataTable dt = new DataTable();
+                    ad.Fill(dt);
+
+                    dataGridView1.DataSource = dt;
+                }
+                con.Close();
+            }
+        }
+    }
+}
--- Data_security/Data_security/Form1.cs
+++ Data_security/Data_security/Form1.cs
@@ -7,14 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+using System.Xml.Linq;
+using System.Security.Cryptography;
+//using Oracle.DataAccess.Client;
 namespace Data_Security
 {
     public partial class Form1 : Form
     {
+        string SERVICE_NAME = null;
+        string id = null;
+        string password = null;
+        string connectString = null;    
         public Form1()
         {
             InitializeComponent();
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SERVICE_NAME = "XE";
+                id = textBox1.Text;
+                Console.WriteLine(id);
+                password = textBox2.Text;
+                Console.WriteLine(password);
+                //connectString = "Data Source=(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = khainehaha)(PORT = 1521)) (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME = XE)));User Id = " + id + ";password =" + password + ";";
+                connectString = $"Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=" + SERVICE_NAME + ")));User Id=" + id + ";Password=" + password + ";DBA Privilege = SYSDBA;" ;
+                
+                using (OracleConnection con = new OracleConnection(connectString))
+                {
+                    con.Open();
+                    if (con.State == ConnectionState.Open)
+                    {
+                        MessageBox.Show("Kết nối thành công!", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        con.Close();
+                        this.Hide();
+                        SuccessLogin successLogin = new SuccessLogin(connectString);
+                        successLogin.ShowDialog();
+                        this.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Kết nối không thành công!", "Thông Báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đăng Nhập Thất Bại: " + ex.Message, "Thất Bại", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
