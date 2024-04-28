d9eaf63bebdd27d3dc063d159b439d32566c28e0
THKhai <116781770+THKhai@users.noreply.github.com>
2024-04-28 14:29:47 +0700

    update cs1

--- PhanHe02/cs/cs01.sql
+++ PhanHe02/cs/cs01.sql
@@ -1,35 +1,41 @@
--- create user
--- cs01
-alter session set "_ORACLE_SCRIPT" =true;
+---- create user
+---- cs01
+--
+--
+connect ADMINLC/ADMINLC;
+--
 create role R_NhanSu;
-create or replace view V_NhanSU
+create or replace view V_NhanSu
 as
     select*
-    from NhanSu
+    from ADMINLC.NhanSu
     where manv = sys_context('USERENV', 'SESSION_USER');
     
     
-grant select,update(DT) on V_NhanSu to R_NhanSu;
-grant select on SINHVIEN to R_NhanSu;
-grant select on DONVI to R_NhanSu;
-grant select on HOCPHAN to R_NhanSu;
-grant select on KHMO to R_NhanSu;
+grant select,update(DT) on  ADMINLC.V_NhanSu to R_NhanSu;
+grant select on ADMINLC.SINHVIEN to R_NhanSu;
+grant select on ADMINLC.DONVI to R_NhanSu;
+grant select on ADMINLC.HOCPHAN to R_NhanSu;
+grant select on ADMINLC.KHMO to R_NhanSu;
 
-create or replace procedure GRANT_ROLE_NS
-as
-   CURSOR CUR IS (SELECT manv
-                    FROM nhansu);
-    STRSQL VARCHAR(2000);
-    USR VARCHAR2(10);
-begin
-    OPEN CUR;
-    LOOP
-        FETCH CUR INTO USR;
-        EXIT WHEN CUR%NOTFOUND;
-        STRSQL := 'GRANT R_NhanSu TO '||USER;
-        EXECUTE IMMEDIATE(STRSQL);
-    END LOOP;
-    CLOSE CUR;
+CREATE OR REPLACE PROCEDURE GRANT_ROLE_NS AS
+   CURSOR CUR IS 
+      SELECT ns.manv
+      FROM ADMINLC.nhansu ns;
+   STRSQL VARCHAR(2000);
+   USR VARCHAR2(10);
+BEGIN
+   OPEN CUR;
+   LOOP
+      FETCH CUR INTO USR;
+      EXIT WHEN CUR%NOTFOUND;
+      STRSQL := 'GRANT R_NhanSu TO ' || USR;
+      EXECUTE IMMEDIATE STRSQL;
+   END LOOP;
+   CLOSE CUR;
 END;
 /
+--
 exec GRANT_ROLE_NS;
+
+
