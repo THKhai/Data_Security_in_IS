9e88cf2743f634f6dd38062126307b81074fbb2f
BCKhang <138310391+BCKhang@users.noreply.github.com>
2024-05-01 09:02:45 +0700

    Khang

--- Data_Security/Data_Security/TRUONGKHOA/TRUONGKHOA.Designer.cs
+++ Data_Security/Data_Security/TRUONGKHOA/TRUONGKHOA.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+<<<<<<< Updated upstream
             this.SuspendLayout();
             // 
             // TRUONGKHOA
@@ -38,10 +39,34 @@
             this.Name = "TRUONGKHOA";
             this.Text = "TRUONGKHOA";
             this.Load += new System.EventHandler(this.TRUONGKHOA_Load);
+=======
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(282, 164);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 41);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "TABLE";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // TRUONGKHOA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Name = "TRUONGKHOA";
+            this.Text = "TRUONGKHOA";
+>>>>>>> Stashed changes
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.Button button1;
     }
 }
